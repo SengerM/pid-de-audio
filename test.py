836f05a3440207f2ee03c6de9bee10281ee114ef
Matias Senger <m.senger@hotmail.com>
2018-10-31 18:38:02 -0300

    Added the "MIN_CHUNK_SIZE" parameter.

--- test.py
+++ test.py
@@ -8,31 +8,24 @@ os.system('clear')
 # Parameters -----------------------------------------------------------
 SIGNAL_FREQUENCY = 1000 # In Hertz.
 SAMPLING_FREQUENCY = 48000 # Must be integer.
+MIN_CHUNK_SIZE = 2**9 # Minimum chunk size.
 # ----------------------------------------------------------------------
 
+pure_samples = np.sin(2*np.pi*np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)*SIGNAL_FREQUENCY/SAMPLING_FREQUENCY)
+while len(pure_samples) < MIN_CHUNK_SIZE:
+	pure_samples = np.append(pure_samples, pure_samples)
+pure_samples = pure_samples.transpose()
+
 def callback(indata, outdata, frames, time, status):
-	outdata[:] = np.sin([2*np.pi*np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)*SIGNAL_FREQUENCY/SAMPLING_FREQUENCY]).transpose()
+	outdata[:] = pure_samples.reshape(len(pure_samples),1)
+	
 
 stream = sd.Stream(
 	samplerate=SAMPLING_FREQUENCY, 
 	callback=callback, 
-	blocksize=len(np.sin(2*np.pi*np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)*SIGNAL_FREQUENCY/SAMPLING_FREQUENCY)), 
+	blocksize=len(pure_samples), 
 	channels=1)
 
 stream.start()
-time.sleep(2)
+time.sleep(1)
 stream.stop()
-
-# ~ samples = np.sin(2*np.pi*np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)*SIGNAL_FREQUENCY/SAMPLING_FREQUENCY)
-# ~ # Create the output samples ---------------------
-# ~ output_samples = samples
-# ~ for k in range(N_CYCLES-1):
-	# ~ output_samples = np.append(output_samples, samples)
-# ~ # Play and record samples -----------------------
-# ~ sd.play(output_samples, SAMPLING_FREQUENCY, loop=True)
-# ~ recorded_samples = sd.rec(int(1*SAMPLING_FREQUENCY), samplerate=SAMPLING_FREQUENCY, channels=2)
-# ~ recorded_samples = np.transpose(recorded_samples)
-# ~ time_axis = np.linspace(0,N_CYCLES/SIGNAL_FREQUENCY,len(recorded_samples[0]))
-
-# ~ nq.plot(time_axis, recorded_samples[0])
-# ~ nq.show()
